68596d6dcc48b2ad2c565f098497effcd9140f98
brian kipkosgei <brianktoo1998@gmail.com>
2024-05-20 17:03:13 +0300

    added flask calculator, with add_app done

--- add_app/__init__.py
+++ add_app/__init__.py
@@ -2,10 +2,5 @@ from flask import Blueprint
 addition_bp = Blueprint('addition_bp', __name__)
 from . import routes
 
-# def  create_app(app):
-#     app = Flask(__name__)
 
-#     with app.app_context():
-#         from. import routes
-#     return app    
 
